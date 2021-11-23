e4bc7381a261f35af18933633194bad1b23b4b89
MartinRenteria <renteriamp96@gmail.com>
2021-11-23 15:27:35 -0800

    Adding static serialNumber for scooter Class

--- src/scooter.js
+++ src/scooter.js
@@ -1,16 +1,19 @@
 class Scooter {
-    constructor(serialNumber, station, user) {
-        this.serialNumber = Math.floor(Math.random() * 100)
+    static serialNumber = 100
+    constructor(station, user) {
+        this.serialNumber = Scooter.serialNumber
+        Scooter.serialNumber+= 1
         this.station = station
         this.user = user 
     }
     recharge() {
-
+        return console.log("")
     }
 }
 
-module.exports = Scooter
+module.exports = Scooter;
 
-const s1 = new Scooter()
-
-console.log(s1.serialNumber)
+const scooter1 = new Scooter('main','martin');
+const scooter2 = new Scooter('main','Pretti');
+console.log(scooter1.serialNumber);
+console.log(scooter2.serialNumber);
